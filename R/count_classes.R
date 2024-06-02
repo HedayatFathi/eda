f8246f8bf51a6d48863e8a7b8d206c7edc29ffcc
Hedayat Fathi <hedayat.fatthi@gmail.com>
2024-06-02 11:01:23 -0230

    independency

--- R/count_classes.R
+++ R/count_classes.R
@@ -1,3 +1,16 @@
+
+#' Title
+#'
+#' @param data_frame
+#' @param class_col
+#'
+#' @return
+#' #'   The first column (named class) lists the classes from the input data frame.
+#'   The second column (named count) lists the number of observations for each class from the input data frame.
+#'   It will have one row for each class present in input data frame.
+#' @export
+#'
+#' @examples
 count_classes <- function(data_frame, class_col) {
   if (!is.data.frame(data_frame)) {
     stop("`data_frame` should be a data frame or data frame extension (e.g. a tibble)")
